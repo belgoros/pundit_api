5e921a68288cf150cdcb65c229468afad761cdff
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-10-25 11:42:12 +0200

    Refactor ApplicationController to use pundit - include Pundit module - include ActionController::MimeResponds - rescue_from Pundit::NotAuthorizedError - add #pundit_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  # In some cases your controller might not have access to current_user,
+  # or your current_user is not the method that should be invoked by Pundit
+  def pundit_user
+    User.first
+  end
+
+  def user_not_authorized
+    render jsonapi: errors_response, status: :unathorized
+  end
+
+  def errors_response
+    {
+      errors:
+      [
+        { message: 'You are not authorized to perform this action.' }
+      ]
+    }
+  end
 end
