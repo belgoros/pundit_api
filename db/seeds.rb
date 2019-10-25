eafdea3956a94db329dafbb76eda66aa332d5fe9
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-10-25 11:15:51 +0200

    Create seeds to populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+(1..3).each do |x|
+  user = User.create!(email: "user-#{x}@example.com", name: "User-#{x}")
+  (1..6).each do |i|
+    user.posts.create(title: "#{user.name}: Title-#{i}", body: "#{i}-fancy content")
+  end
+end
