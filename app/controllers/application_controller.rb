3a25d9ea692721b5bc588cfc1ae92b4fc779bcaa
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-11-25 16:34:06 +0100

    Add resque from ActionController::UnpermittedParameters to ApplicationController
    - add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,28 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include Pundit
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::NotAuthorizedError, ActionController::UnpermittedParameters, with: :user_not_authorized
 
   private
 
   # In some cases your controller might not have access to current_user,
   # or your current_user is not the method that should be invoked by Pundit
   def pundit_user
-    User.first
+    User.find(2)
   end
 
   def user_not_authorized
-    render jsonapi: errors_response, status: :unathorized
+    render jsonapi: errors_response, status: :unauthorized
   end
 
   def errors_response
     {
       errors:
-      [
-        { message: 'You are not authorized to perform this action.' }
-      ]
+        [
+          { message: 'You are not authorized to perform this action.' }
+        ],
+      title:
+        'Invalid Attribute'
     }
   end
 end
