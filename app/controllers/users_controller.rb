abbb36f47e28785a412d3f8873c057527049a5b7
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-12-03 09:26:02 +0100

    Fix Rubocop linter warnings - add # frozen_string_literal: true - add class comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# Controller class to manage Users
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    render jsonapi: @users
+    users = User.all
+    render jsonapi: users
   end
 end
