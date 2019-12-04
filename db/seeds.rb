cc80ca3468710f1f23fd251c102324f0c563e7e5
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-12-04 17:58:01 +0100

    Use Faker to populate data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
+Framework.create(name: "Ruby on Rails")
+Framework.create(name: "Bootstrap")
+Framework.create(name: "Foundation")
+Framework.create(name: "Tapestry")
+Framework.create(name: "Struts-2")
+
+frameworks = Framework.all
+
 (1..3).each do |x|
-  user = User.create!(email: "user-#{x}@example.com", name: "User-#{x}")
+  user = User.create!(email: Faker::Internet.email, name: Faker::Internet.username)
   (1..6).each do |i|
-    user.posts.create(title: "#{user.name}: Title-#{i}", body: "#{i}-fancy content")
+    user.posts.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, framework: frameworks.sample)
   end
 end
