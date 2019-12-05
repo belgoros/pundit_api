02e5c8496b78ae0633f4cb7387e61a8a25fea710
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-12-05 09:55:07 +0100

    Add framework_id, user_id attributes, override #create?

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,9 +1,13 @@
 class PostPolicy < ApplicationPolicy
   def permitted_attributes
     if user.admin? || user.national?
-      [:title, :body, :framework]
+      [:title, :body, :framework_id, :user_id]
     else
       [:body]
     end
+  end
+
+  def create?
+    user.admin?
   end
 end
